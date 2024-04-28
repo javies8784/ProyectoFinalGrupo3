d1a01317f6b3002158b6babfb63fcf2ffab63a2e
javies8784 <javies8784@gmail.com>
2024-04-27 22:58:08 -0500

    Agregar archivos de proyecto.

---
+++ Vistas/VlistaMultas.xaml.cs
@@ -0,0 +1,9 @@
+namespace ProyectoG3.Vistas;
+
+public partial class VlistaMultas : ContentPage
+{
+	public VlistaMultas()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Vistas/VregistroMultas.xaml.cs
@@ -0,0 +1,29 @@
+namespace ProyectoG3.Vistas;
+
+public partial class VregistroMultas : ContentPage
+{
+	public VregistroMultas()
+	{
+		InitializeComponent();
+	}
+
+    private void btGPS_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void TomarFoto_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btGuardar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btEliminar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
